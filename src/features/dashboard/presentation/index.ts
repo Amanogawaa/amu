183810264b7fb822939efee3eac153a558cae62c
estrella yue <dommolino28@gmail.com>
2026-04-01 20:41:50 +0800

    fix broken import

--- src/features/dashboard/presentation/index.ts
+++ src/features/dashboard/presentation/index.ts
@@ -1,6 +1,4 @@
 export { DashboardStats } from "./DashboardStats";
-export { StreakCalendar } from "./StreakCalendar";
-export { Leaderboard } from "./Leaderboard";
 export { CourseCard } from "./CourseCard";
 export { QuickActions } from "./QuickActions";
 export { default as LearningCourseCard } from "./LearningCourseCard";
